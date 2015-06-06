f340fd48d6982775c7e5e47246c30ce771309066
Mrunal <mrunal@outlook.com>
2015-06-06 11:57:46 -0400

    added config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+var config = {}
+
+config.port = 8080;
+config.CALLBACK_URL = 'http://localhost:8080';
+config.CLIENT_ID = "SFDC_CLIENTID";
+config.CLIENT_SECRET = "SFDC_CLIENTSECRET";
+config.SFDC_USERNAME = "SFDC_USERNAME";
+config.SFDC_PASSWORD = "SFDC_PASSWORD";
+config.SFDC_SECURITYTOKEN = "SFDC_SECURITYTOKEN";
+
+//  Basic Authentication credentials  
+config.ELOQUA_USERNAME = "ELOQUA_USERNAME";  
+config.ELOQUA_PASSWORD = "ELOQUA_PASSWORD";  
+config.ELOQUA_URL = "ELOQUA_URL";
+
+
+module.exports = config;
